1fdea9031aabb8d3ddb2e19f32854608cb3ef1a2
LoicDesserich <loic.desserich@hotmail.com>
2022-04-15 11:28:49 +0200

    pop-up dépot

--- src/View/DepotPopUp.java
+++ src/View/DepotPopUp.java
@@ -1,8 +1,37 @@
 package View;
 
-public class DepotPopUp {
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-	public DepotPopUp() {
+public class DepotPopUp {
+	JComboBox choixCompte;
+	JTextField txtMontantDepot;
+	JTextField txtSolde;
+	Object[] popUpTab;
+	JPanel container;
+	
+	public DepotPopUp(JPanel container) {
+		this.container = container;
 		
+		JLabel lblChoixCompte = new JLabel("Choix du compte :");
+		JLabel lblMontantDepot = new JLabel("Montant du dépot :");
+		JLabel lblSolde = new JLabel("Solde aprés opération :");
+	
+		choixCompte = new JComboBox();	
+		txtMontantDepot = new JTextField();
+		txtSolde = new JTextField();
+			
+		popUpTab = new Object[] {lblChoixCompte, choixCompte, lblMontantDepot, txtMontantDepot, lblSolde, txtSolde};
+	}
+	
+	public void showPopUp() {
+		int choix = JOptionPane.showConfirmDialog(container, popUpTab, "Dépot", JOptionPane.OK_CANCEL_OPTION);
+		
+		if (choix == JOptionPane.OK_OPTION) {
+			System.out.println("it's okay");
+		}
 	}
 }
